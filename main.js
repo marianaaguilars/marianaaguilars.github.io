b634e7728b7b89ca69e64f5ed1d391c3f0ecd020
marianaaguilars <119458540+marianaaguilars@users.noreply.github.com>
2023-03-05 16:10:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color:#00abe3;">Desarrollo sitios web y escribo artículos sobre programación.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+
